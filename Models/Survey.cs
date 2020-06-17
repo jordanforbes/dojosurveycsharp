5bc7de37a6bf7e7688c27ed481860e9bbefdef86
jordanforbes <jordforb@gmail.com>
2020-06-17 13:51:11 -0400

    added validations

--- Models/Survey.cs
+++ Models/Survey.cs
@@ -1,12 +1,21 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DojoSurvey.Models
 {
     public class Survey
     {
+        [Required]
+        [MinLength(2, ErrorMessage="Name must be at least 2 characters")]
         public string Name {get;set;}
+
+        [Required]
         public string Location {get;set;}
+
+        [Required]
         public string FavLang {get;set;} 
+
+        [MinLength(20, ErrorMessage="Comment must have at least 20 characters")]
         public string Comment {get;set;}
     }
 }
